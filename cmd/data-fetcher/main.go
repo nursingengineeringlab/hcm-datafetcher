a017c054990ec64f3cc7655b0cffd901414447c9
Shiyang Wang <shiyangwang@umass.edu>
2022-03-10 19:17:51 -0500

    adding data fetcher

--- cmd/data-fetcher/main.go
+++ cmd/data-fetcher/main.go
@@ -1,27 +1,103 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
+	"github.com/golang/protobuf/proto"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
+	redistimeseries "github.com/RedisTimeSeries/redistimeseries-go"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	pb "github.com/shiywang/hcm-datafetcher/proto-gen/github.com/shiywang/hcm-datafetcher"
 )
 
-type User struct {
-	bun.BaseModel `bun:"table:users,alias:u"`
+var redisClient *redistimeseries.Client
 
-	ID   int64 `bun:",pk,autoincrement"`
-	Name string
+// https://github.com/RedisTimeSeries/RedisTimeSeries
+// https://github.com/RedisTimeSeries/redistimeseries-go/
+func dataInsert(deviceID string, dataPoint float64) {
+	_, haveIt := redisClient.Info(deviceID)
+	if haveIt != nil {
+		redisClient.CreateKeyWithOptions(deviceID, redistimeseries.DefaultCreateOptions)
+		redisClient.CreateKeyWithOptions(deviceID+"_avg", redistimeseries.DefaultCreateOptions)
+		redisClient.CreateRule(deviceID, redistimeseries.AvgAggregation, 60, deviceID+"_avg")
+	}
+	// Add sample with timestamp from server time and value 100
+	// TS.ADD mytest * 100
+	_, err := redisClient.AddAutoTs(deviceID, dataPoint)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println("Insert data successfully.")
+}
+
+type JsonData struct {
+	dp []redistimeseries.DataPoint
+}
+
+func dataQuery(deviceID string, endTime int64) []redistimeseries.DataPoint {
+	var hour24 int64 = 86400000
+	dataPoints, _ := redisClient.RangeWithOptions(deviceID, endTime-hour24, endTime, redistimeseries.DefaultRangeOptions)
+	return dataPoints
+}
+
+var httpQueryHandler = func(w http.ResponseWriter, req *http.Request) {
+	//io.WriteString(w, "Hello, world!\n")
+
+	var deviceID string
+	var endTime int64
+	for k, v := range req.URL.Query() {
+		fmt.Printf("%s: %s\n", k, v)
+		if k == "deviceId" {
+			deviceID = v[0]
+			continue
+		}
+		if k == "endTime" {
+			endTime, _ = strconv.ParseInt(v[0], 10, 64)
+			continue
+		}
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	type User struct {
+		Id      string
+		Balance uint64
+	}
+	//u := User{Id: "US123", Balance: 8}
+	data := dataQuery(deviceID, endTime)
+
+	js := JsonData{}
+	counter := 1
+	for _, v := range data {
+		js.dp = append(js.dp, v)
+		if counter == 500 {
+			break
+		}
+		counter++
+	}
+
+	json.NewEncoder(w).Encode(js.dp)
 }
 
 var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("TOPIC: %s\n", msg.Topic())
-	fmt.Printf("MSG: %s\n", msg.Payload())
-
+	//fmt.Printf("TOPIC: %s\n", msg.Topic())
+	//fmt.Printf("MSG: %s\n", msg.Payload()).
+	packet := &pb.ECGPacket{}
+	if err := proto.Unmarshal(msg.Payload(), packet); err != nil {
+		log.Fatalln("Failed to parse address book:", err)
+	}
+	fmt.Println(packet)
+	if packet.DataType == pb.ECGPacket_RRI {
+		dataInsert(packet.DeviceId, float64(packet.Value))
+	}
 }
 
 func main() {
@@ -29,7 +105,7 @@ func main() {
 
 	mqtt.DEBUG = log.New(os.Stdout, "", 0)
 	mqtt.ERROR = log.New(os.Stdout, "", 0)
-	opts := mqtt.NewClientOptions().AddBroker("tcp://broker.emqx.io:1883").SetClientID("emqx_data_fetcher")
+	opts := mqtt.NewClientOptions().AddBroker("tcp://172.24.41.85:1883").SetClientID("emqx_data_fetcher")
 
 	opts.SetKeepAlive(60 * time.Second)
 	// Set the message callback handler
@@ -38,20 +114,20 @@ func main() {
 
 	c := mqtt.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-
-	// Subscribe to a topic
-	if token := c.Subscribe("testtopic/#", 0, nil); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+	// Subscribe to a topic
+	if token := c.Subscribe("emqtt", 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
 	}
 
-	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listing for requests at http://localhost:8000/hello")
+	redisClient = redistimeseries.NewClient("172.24.41.85:6379", "nohelp", nil)
+
+	// http://0.0.0.0:8888/graph?deviceId=ED5A782825AB&endTime=1646945822002
+	http.HandleFunc("/graph", httpQueryHandler)
+	log.Println("Listing for requests at http://0.0.0.0:8000/graph")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,44 @@ module github.com/shiywang/hcm-datafetcher
 
 go 1.17
 
-require github.com/eclipse/paho.mqtt.golang v1.3.5
+require (
+	github.com/eclipse/paho.mqtt.golang v1.3.5
+	github.com/uptrace/bun v1.1.1
+	github.com/uptrace/bun/dialect/sqlitedialect v1.1.1
+	github.com/uptrace/bun/driver/sqliteshim v1.1.1
+	github.com/uptrace/bun/extra/bundebug v1.1.1
+)
 
 require (
+	github.com/RedisTimeSeries/redistimeseries-go v1.4.4 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gomodule/redigo v1.8.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/tools v0.1.9 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	lukechampine.com/uint128 v1.2.0 // indirect
+	modernc.org/cc/v3 v3.35.22 // indirect
+	modernc.org/ccgo/v3 v3.15.13 // indirect
+	modernc.org/libc v1.14.5 // indirect
+	modernc.org/mathutil v1.4.1 // indirect
+	modernc.org/memory v1.0.5 // indirect
+	modernc.org/opt v0.1.1 // indirect
+	modernc.org/sqlite v1.14.7 // indirect
+	modernc.org/strutil v1.1.1 // indirect
+	modernc.org/token v1.0.0 // indirect
 )
